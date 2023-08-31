b5efccfb98865cb4f797e50b5e2da97103a1fe65
Hyandro <143363394+Hyandro@users.noreply.github.com>
2023-08-30 23:24:16 -0300

    MarioJump

---
+++ java.js
@@ -0,0 +1,61 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
+
+let loop;
+
+const restartGame = () => {
+    mario.style.bottom = '0';
+    mario.src = './MARIO PNG/mario.gif';
+    mario.style.width = '40px';
+    mario.style.marginLeft = '0';
+
+    pipe.style.animationPlayState = 'running';
+
+    restartButton.style.display = 'none';
+
+    clearInterval(loop);
+    startLoop();
+};
+
+const jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+        mario.classList.remove('jump');
+    }, 600);
+};
+
+const checkCollision = () => {
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
+
+    if (pipePosition <= 100 && pipePosition > 0 && marioPosition < 80) {
+        clearInterval(loop);
+
+        pipe.style.animationPlayState = 'paused';
+        pipe.style.left = `${pipePosition}px`;
+
+        const marioNewPosition = 65; 
+        mario.style.bottom = `${marioNewPosition}px`;
+
+        mario.src = './MARIO PNG/game-over.png';
+        mario.style.width = '70px';
+        mario.style.marginLeft = '50px';
+
+        restartButton.style.display = 'block';
+    }
+};
+
+
+const startLoop = () => {
+    loop = setInterval(() => {
+        checkCollision();
+    }, 10);
+};
+
+startLoop();
+
+document.addEventListener('keydown', jump);
+
+restartButton.addEventListener('click', restartGame);
